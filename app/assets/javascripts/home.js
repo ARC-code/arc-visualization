0bd7152b1deeaa36ebf42fef1fa5a80bd1e59a01
Lou Foster <lou@performantsoftware.com>
2014-02-10 12:12:36 -0500

    work on popup info for nodes & menu

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -26,7 +26,6 @@ $(function() {
       $("#menu").hide();
    });
 
-
    // Calc charge on node based on size. Bigger nodes repel more
    var calcCharge = function(d) {
       var size = Math.sqrt(d.size) / 10 || 3;
@@ -34,6 +33,7 @@ $(function() {
    };
 
    // Initialize D3 visualization
+   var dragging = false;
    var width = $(window).width();
    var height = $(window).height() - $("#site-header").outerHeight(true) - 20;
    var root;
@@ -43,6 +43,11 @@ $(function() {
    var link = svg.selectAll(".link");
    var node = svg.selectAll(".node");
 
+   // for title popup tooltip
+   var tipShowTimer = -1;
+   var tipX;
+   var tipY;
+
    // request the initial set of data; the archives
    d3.json("/archives", function(json) {
       root = json;
@@ -50,7 +55,8 @@ $(function() {
    });
 
    // handle nodes being dragged
-   var drag = force.drag().on("drag", onDragStart);
+   var drag = force.drag().on("dragstart", onDragStart);
+   force.drag().on("dragend", function() {dragging = false;});
 
    // Redraw the d3 graph based on JSON data
    function updateArchives() {
@@ -90,11 +96,7 @@ $(function() {
       node.exit().remove();
 
       // Enter any new nodes.
-      var circle = node.enter().append("circle").attr("class", "node").attr("cx", function(d) {
-         return d.x;
-      }).attr("cy", function(d) {
-         return d.y;
-      }).attr("r", function(d) {
+      var circle = node.enter().append("circle").attr("class", "node").attr("r", function(d) {
          if (d.name === "ARC Catalog") {
             d3.select(this).classed("root", true);
             return 20;
@@ -103,9 +105,12 @@ $(function() {
             return 10;
          }
          return Math.max(Math.sqrt(d.size) / 10 || 4.5, 5);
-      }).style("fill", color).classed("fixed", isFixed).on("click", click).on("dblclick", onDoubleClick).call(drag);
-
-      circle.append("svg:title").text(nodeTitle);
+      }).style("fill", color).classed("fixed", isFixed)
+         .on("click", click)
+         .on("dblclick", onDoubleClick)
+         .on("mouseenter", onMouseOver)
+         .on("mouseleave", onMouseLeave)
+         .call(drag);
    }
 
    function tick() {
@@ -153,25 +158,52 @@ $(function() {
       }
    }
 
-   function nodeTitle(d, i) {
-      if (d.size) {
-         return d.name + ": " + d.size;
-      } else {
-         return d.name;
+   function onMouseOver(d) {
+      if (dragging == false && $("#menu").is(":visible") == false) {
+         var tipTarget = d;
+         tipX = d3.event.pageX + 10;
+         tipY = d3.event.pageY + 10;
+         if (tipShowTimer === -1) {
+            tipShowTimer = setTimeout(function() {
+               $("#info .title").text(d.name);
+               $("#info .size").text(d.size);
+               $("#info").css({
+                  "top" : d.y + "px",
+                  "left" : d.x + "px"
+               });
+               $("#info").fadeIn();
+            }, 750);
+         }
+
       }
+   }
+
+   function onMouseLeave(d) {
+      if (tipShowTimer !== -1) {
+         clearTimeout(tipShowTimer);
+         tipShowTimer = -1;
+      }
+      $("#info").fadeOut();
    }
 
    // Handle click on a node; configure and display the menu
    function click(d) {
       if (!d3.event.defaultPrevented) {
+         if (tipShowTimer !== -1) {
+            clearTimeout(tipShowTimer);
+            tipShowTimer = -1;
+         }
+
+         var collapsed = false;
+         $("#expand").hide();
+         $("#collapse").hide();
          if (d.children) {
             $("#collapse").show();
-            $("#expand").hide();
-         } else {
+         } else if ( d._children) {
             $("#expand").show();
-            $("#collapse").hide();
+            collapsed = true;
          }
-         $("#menu").show();
+         $("#menu").fadeIn();
          $("#menu").offset({
             top : d.y + 10,
             left : d.x+10
@@ -181,6 +213,16 @@ $(function() {
          d.fixed = true;
          d3.select(this).classed("fixed", true);
          d3.event.stopPropagation();
+
+         $("#genre").hide();
+         $("#discipline").hide();
+         $("#format").hide();
+         if ( d.name !== "ARC Catalog" && !collapsed ) {
+            // TODO
+            $("#genre").show();
+            $("#discipline").show();
+            $("#format").show();
+         }
       }
    }
 
@@ -192,6 +234,7 @@ $(function() {
 
    function onDragStart(d) {
       $("#menu").hide();
+      dragging = true;
       d3.select(this).classed("fixed", d.fixed = true);
    };
 
