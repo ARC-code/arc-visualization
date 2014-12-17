e8bb799565045b8b73fce0bd878dc707467c9f66
Ed Zavada <ed.zavada@gmail.com>
2014-12-17 17:53:00 -0500

    added some extra stuff to start script to ensure it works with RVM regardless of how login was done

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
 bundle exec unicorn_rails -c config/unicorn.rb -E production -D
 ps aux | grep unicorn | grep -v grep
