e8be8c0c28199e15024a226832ff35eb22e7aece
nicklaiacona <nick@performantsoftware.com>
2016-07-28 16:06:43 -0400

    some implementation notes

---
+++ app/assets/javascripts/intro_dialog.js
@@ -0,0 +1,6 @@
+// if cookie is not set, then display the intro dialog
+
+// intro dialog is a multistep wizard which maintains constant size and a position in center of display. It may be dismissed at any time.
+
+// if don't show this again is checked, set the cookie
+
