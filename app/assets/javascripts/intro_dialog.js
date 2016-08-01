62200086917df7a4a742a74819e0686e0feb6658
nicklaiacona <nick@performantsoftware.com>
2016-08-01 12:54:46 -0400

    add positioning and resizing code

--- app/assets/javascripts/intro_dialog.js
+++ app/assets/javascripts/intro_dialog.js
@@ -4,3 +4,56 @@
 
 // if don't show this again is checked, set the cookie
 
+var BigDIVA = {};
+	
+BigDIVA.IntroDialog = function() {
+    this.initialize.apply(this, arguments);
+};
+
+_.extend( BigDIVA.IntroDialog.prototype, {
+
+  maxWidth: 3600,
+  maxHeight: 1600,
+  margin: 200,
+  padding: 50,
+                        
+	initialize: function() {			    
+    _.bindAll( this, 'resizeDialog');
+	},
+  
+  resizeDialog: function() {
+    var windowWidth = $(window).width();
+    var windowHeight = $(window).height();
+    
+    var dialogWidth, dialogHeight;
+    if( windowWidth >= this.maxWidth ) {
+      dialogWidth = this.maxWidth;
+      dialogHeight = this.maxHeight;      
+    } else {
+      dialogWidth = windowWidth - (this.margin*2);
+      dialogHeight = this.maxHeight * (dialogWidth/this.maxWidth);
+    }
+    
+    var introDialog = $('#intro-dialog');
+    introDialog.width(dialogWidth);
+    introDialog.height(dialogHeight);
+    introDialog.offset({ top: this.margin, left: this.margin });
+    
+    var introStep = $('.intro-step');
+    introStep.width(dialogWidth-this.padding);
+    introStep.height(dialogHeight-this.padding);
+  },
+             
+	render: function() {
+    
+    // dialog needs to be positioned and sized based on size of window
+    
+    // scale factor that scales all graphics proportionally based on width
+    this.resizeDialog();
+
+		// track on window resize
+		$(window).resize(this.resizeDialog);
+		
+	}
+  					
+});
